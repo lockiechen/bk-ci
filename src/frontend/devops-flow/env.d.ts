a2d3b89c406a3ead60d38ae478512b0762a0fe5e
lockiechen <lockiechen@tencent.com>
2026-01-06 13:02:02 +0800

    feat: flow

--- src/frontend/devops-flow/env.d.ts
+++ src/frontend/devops-flow/env.d.ts
@@ -1,19 +1,16 @@
 /// <reference types="vite/client" />
 
 declare module 'bkui-vue/dist/locale/zh-cn.esm' {
-  import { ComponentCustomProperties } from 'vue'
-  const locale: any
+    const locale: any
   export default locale
 }
 
 declare module 'bkui-vue/dist/locale/en.esm' {
-  import { ComponentCustomProperties } from 'vue'
-  const locale: any
+    const locale: any
   export default locale
 }
 
 declare module 'bkui-vue/dist/locale/ja-jp.esm' {
-  import { ComponentCustomProperties } from 'vue'
-  const locale: any
+    const locale: any
   export default locale
 }
